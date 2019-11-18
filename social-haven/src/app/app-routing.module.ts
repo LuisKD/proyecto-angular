858ba317fffb639439da63a94b5c769575975257
luiskd2 <luis.pedraza.lpm@gmail.com>
2019-11-18 20:32:24 +0100

    .

--- social-haven/src/app/app-routing.module.ts
+++ social-haven/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MisHistorietasComponent } from './mis-historietas/mis-historietas.component';
+import { MisCosasComponent } from './mis-cosas/mis-cosas.component';
+import { MisColegasComponent } from './mis-colegas/mis-colegas.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, // Equivale a: {path: '', component: ItemComponent} --> http://localhost:4200/
+  {path: 'home', component: MisHistorietasComponent}, // http://localhost:4200/home
+  {path: 'mis-historietas', component: MisHistorietasComponent},
+  {path: 'mis-cosas', component: MisCosasComponent},
+  {path: 'mis-colegas', component: MisColegasComponent},
+  {path: 'eventos', component: EventosComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
--- social-haven/src/app/app.module.ts
+++ social-haven/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MisHistorietasComponent } from './mis-historietas/mis-historietas.component';
+import { MisCosasComponent } from './mis-cosas/mis-cosas.component';
+import { MisColegasComponent } from './mis-colegas/mis-colegas.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MisHistorietasComponent,
+    MisCosasComponent,
+    MisColegasComponent,
+    EventosComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
